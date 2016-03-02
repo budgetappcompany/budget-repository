6b7aedc55cd78d3ba4f6635bb177c8f38b1f070e
Calebe Santos <budgetcompany@gmail.com>
2016-03-01 23:43:56 -0300

    Vota por favor

---
+++ Budget/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Budget
+//
+//  Created by Yuri Pereira on 3/1/16.
+//  Copyright © 2016 Budget. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    // Menu
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
